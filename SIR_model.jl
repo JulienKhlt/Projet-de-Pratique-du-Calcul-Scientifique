28510bee50f7f673b891be692e6356bbeb0cde0e
Julien KHLAUT <julien.khlaut@eleves.enpc.fr>
2020-04-15 14:16:24 +0200

    Vector initialize

--- SIR_model.jl
+++ SIR_model.jl
@@ -14,6 +14,11 @@ pas_tps = 0.1
 tps = 40
 T = range(0, tps, length = Int(tps/pas_tps))
 
+function f(U, β, γ, N)
+    dUdt = [- β*U[2]*U[1]/N, β*U[2]*U[1]/N - γ*U[2], γ*U[2]]
+    return dUdt
+end
+
 function epidemie(S, I, R, β, γ, pas_tps, tps)
 
     Susceptible = Float64[S]
@@ -21,21 +26,20 @@ function epidemie(S, I, R, β, γ, pas_tps, tps)
     Recovered = Float64[R]
 
     N = S + I + R
+    U = Float64[S, I, R]
 
     for t in range(pas_tps, tps, length = Int(tps/pas_tps))
-        dSdt = - β*I*S/N
-        dIdt = β*I*S/N - γ*I
-        dRdt = γ*I
+        dUdt = f(U, β, γ, N)
 
-        S += dSdt*pas_tps
-        I += dIdt*pas_tps
-        R += dRdt*pas_tps
+        U += dUdt * pas_tps
 
-        append!(Susceptible, S)
-        append!(Infectious, I)
-        append!(Recovered, R)
+        append!(Susceptible, U[1])
+        append!(Infectious, U[2])
+        append!(Recovered, U[3])
     end
+
     T = range(0, tps, length = Int(tps/pas_tps)+1)
+    
     plot(T, Susceptible, color="#4a4c4d", linewidth=2.0, linestyle="-", label="Susceptible")
     plot(T, Infectious, color="#c13607", linewidth=2.0, linestyle="-", label="Infectious")
     plot(T, Recovered, color="#3c9f66", linewidth=2.0, linestyle="-", label="Recovered")
