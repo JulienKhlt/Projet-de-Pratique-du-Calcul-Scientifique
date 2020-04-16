fab49d8e24663702776f34a12abf034defd5c23c
Julien KHLAUT <julien.khlaut@eleves.enpc.fr>
2020-04-16 12:17:56 +0200

    Failed

--- Diffusion_model.jl
+++ Diffusion_model.jl
@@ -1,3 +1,5 @@
+using PyPlot
+
 Nombre_maille = 100
 x = range(0, 1, length=Nombre_maille)
 dx = 1/Nombre_maille
@@ -15,7 +17,7 @@ end
 U[2][50] = 1
 
 function f(U, β, γ, N)
-    dUdt = [- β*U[2]*U[1]/N, β*U[2]*U[1]/N - γ*U[2], γ*U[2]]
+    dUdt = [- β*U[2].*U[1]/N, β*U[2].*U[1]/N - γ*U[2], γ*U[2]]
     return dUdt
 end
 
@@ -26,12 +28,23 @@ tps = 10
 β = 1
 γ = 0.25
 
-N = Nombre_maille + 1
-
-for t in range(pas_tps, tps, length = Int(tps/pas_tps))
-    for k in 1:Nombre_maille
-        for i in 1:length(U)
-        laplacien[i][k] = (U[i][k+1]-2*U[i][k]+U[i][k-1])/(dx^2)
+function diffusion(U, β, γ, D, dx, pas_tps, tps)
+    Epidemie = [U]
+    N = Nombre_maille + 1
+    for t in range(pas_tps, tps, length = trunc(Int, (tps/pas_tps)))
+        for k in 2:Nombre_maille-1
+            for i = 1:length(U)
+            laplacien[i][k] = (U[i][k+1]-2*U[i][k]+U[i][k-1])/(dx^2)
+            end
+        end
+        U .+= pas_tps*(D*laplacien + f(U, β, γ, N)) 
+        append!(Epidemie, [U])
+        println(U)
+        
     end
-    U += pas_tps*(D*laplacien + f(U, β, γ, N)) 
+    println(Epidemie)
 end
+
+diffusion(U, β, γ, D, dx, pas_tps, tps)
+
+# f(U, β, γ, Nombre_maille+1)
